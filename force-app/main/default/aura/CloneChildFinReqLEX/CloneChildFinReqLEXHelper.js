8d84d133ec24e197ad8d4e8f3525b19679ca8a21
vijaykumar96 <vijaykumar.kr723@gmail.com>
2020-05-28 18:58:51 +0530

    RT-588: Removed inserting DML & handling in JS

--- force-app/main/default/aura/CloneChildFinReqLEX/CloneChildFinReqLEXHelper.js
+++ force-app/main/default/aura/CloneChildFinReqLEX/CloneChildFinReqLEXHelper.js
@@ -1,3 +1,13 @@
 ({
-   
+    showToast : function(component, event,title,type,message) {
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            "title": title,
+            "type" : type,
+            "message": message
+        });
+        toastEvent.fire();
+       
+    }
+    
 })
