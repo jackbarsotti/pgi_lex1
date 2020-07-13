bf5f87ac93047a909c7b49a28c7d792f94a08e3c
vijaykumar96 <vijaykumar.kr723@gmail.com>
2020-07-13 18:51:59 +0530

    iContract: removed lightningstylesheet July 13th

--- force-app/main/default/lwc/iContractLex/iContractLex.js
+++ force-app/main/default/lwc/iContractLex/iContractLex.js
@@ -2,9 +2,9 @@ import { LightningElement, api ,track} from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 export default class IContractLex extends NavigationMixin (LightningElement) {
     @api recordId;
+    @track url
     
-      get Url(){
+    get Url(){
         return '/apex/iContract_NA_Master?id='+this.recordId;
-         
     }
 }
