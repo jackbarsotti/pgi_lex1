f06d39e2b7c2b194ef868dbb69ffc300ec7b967f
Jack Barsotti <jackbarsotti1@gmail.com>
2020-08-06 17:13:18 -0500

    test

--- deploy.sh
+++ deploy.sh
@@ -86,7 +86,8 @@ if [ "$BRANCH" == "master" ]; then
   # ls force-app/main/default, then loop through that, then go through each folder and subtype of file, git diff
   #ls force-app/main/default |
     #while 
-  export CHANGED_FILES=$(git diff --name-only LEX force-app/)
+  #export CHANGED_FILES=$(git diff --name-only LEX force-app/)
+  export CHANGED_FILES=$(git diff --name-only LEX force-app/main/default/classes)
   #for f in $CHANGED_FILES; do
     #sudo cp --parents $f $DEPLOYDIR;
   #done;
@@ -114,7 +115,7 @@ for FILE in $CHANGED_FILES; do
   if [[ $FILE == *Test.cls ]]; then
     #find $classPath -maxdepth1 -samefile "$FILE-meta.xml" -exec sudo cp --parents "{}" $DEPLOYDIR +
     #mcp "*Test.cls" "#1copy#2"
-    #find $classPath -samefile "$FILE-meta.xml" | xargs -n 1000 cp --parents {} $DEPLOYDIR
+      #find $classPath -samefile "$FILE-meta.xml" | xargs -n 1000 cp --parents {} $DEPLOYDIR
     #find $classPath -samefile '$FILE-meta.xml' -exec cp -p {} $DEPLOYDIR \;
     #strace -f -v -s 99999999 -o strace.log find $classPath -samefile "$FILE-meta.xml" | xargs --max-args=1 cp --parents {} $DEPLOYDIR
     #find "$FILE-meta.xml" -name | xargs cp $DEPLOYDIR
@@ -123,7 +124,7 @@ for FILE in $CHANGED_FILES; do
     #find $classPath -samefile "$FILE-meta.xml" -maxdepth1 -exec /bin/cp --parents {} $DEPLOYDIR +
     #PAGE_SIZE*MAX_ARG_PAGES-sizeof(void *) / sizeof(void *)
     #find $classPath -samefile "$FILE-meta.xml" -exec sudo cp --parents -t $DEPLOYDIR {} +
-    sudo cp --parents "$(find $classPath -samefile "$FILE-meta.xml")"* $DEPLOYDIR;
+      #sudo cp --parents "$(find $classPath -samefile "$FILE-meta.xml")"* $DEPLOYDIR;
     #printf "%s\0" * | parallel --dry-run -0 cp -p "{}" $DEPLOYDIR
   fi;
 done;
