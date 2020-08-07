70eac31c33c7e3ec09c3e34feee6f11c67e669b9
Jack Barsotti <jackbarsotti1@gmail.com>
2020-08-07 15:07:45 -0500

    automation testing

--- deploy.sh
+++ deploy.sh
@@ -81,10 +81,12 @@ if [ "$BRANCH" == "master" ]; then
     if [[ $output == "cp: failed to get attributes of '$file': No such file or directory" ]]; then
       sudo mkdir $file
       sudo cp "$file" /Users/timbarsotti/pgi_lex/force-app/main/default/diff
+      whoami
     elif [[ $output == "cp: cannot stat '$file': No such file or directory" ]]; then
       #string=echo ${$file:19} | sudo touch $string
       touch $file
       sudo cp --parents "$file" /Users/timbarsotti/pgi_lex/force-app/main/default/diff
+      whoami
     fi
     if [[ $file == *.cls ]]; then
       find force-app/main/default/classes -samefile "$file-meta.xml" -exec sudo cp --parents -t /Users/timbarsotti/pgi_lex/force-app/main/default/diff {} +
