b01e7b8e9e2015301a4a4d0b03de16f2552aa695
Jack Barsotti <jackbarsotti1@gmail.com>
2020-08-07 13:46:53 -0500

    automation testing

--- deploy.sh
+++ deploy.sh
@@ -81,7 +81,7 @@ if [ "$BRANCH" == "master" ]; then
       sudo mkdir $file
       sudo cp --parents "$file"  /Users/timbarsotti/pgi_lex/force-app/main/default/diff
     elif [[ $output == "cp: cannot stat '$file': No such file or directory" ]]; then
-      echo ${$file:19} | ${$file:19}=string | sudo touch $string
+      string=echo ${$file:19} | sudo touch $string
       sudo cp --parents "$file"  /Users/timbarsotti/pgi_lex/force-app/main/default/diff
     fi
     if [[ $file == *.cls ]]; then
