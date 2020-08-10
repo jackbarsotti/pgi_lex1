b55d05cae4f6722395f78de953ed7028ab9c9cea
Jack Barsotti <jackbarsotti1@gmail.com>
2020-08-10 13:36:47 -0500

    automation testing cannot stat

--- deploy.sh
+++ deploy.sh
@@ -79,12 +79,12 @@ if [ "$BRANCH" == "master" ]; then
     echo $file
     sudo cp --parents "$file" /Users/jackbarsotti/pgi_lex1/force-app/main/default/diff
     export output=$(sudo cp --parents "$file" /Users/jackbarsotti/pgi_lex1/force-app/main/default/diff)
-    echo $output | if [[ $output == "cp: failed to get attributes of '$file': No such file or directory" ]]; then
+    if [[ $output == "cp: failed to get attributes of '$file': No such file or directory" ]]; then
       # need to remove the ending before mkdir
       # originally: sudo mkdir $file
       #sudo mkdir -p $file
       #sudo cp "$file" /Users/jackbarsotti/pgi_lex1/force-app/main/default/diff
-      exit 
+      set -e 
     #elif
     elif [[ $output == "cp: cannot stat '$file': No such file or directory" ]]; then
       touch $file
