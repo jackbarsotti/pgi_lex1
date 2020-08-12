f6eb219ff092956290750b11965f63bf5ac9926d
Jack Barsotti <jackbarsotti1@gmail.com>
2020-08-12 11:55:30 -0500

    matched with sfdx repo

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ export SFDX_DOMAIN_RETRY=300
 export SFDX_DISABLE_APP_HUB=true
 export SFDX_LOG_LEVEL=DEBUG
 echo 'mkdir sfdx...'
-mkdir sfdx 2>/dev/null
+mkdir sfdx
 wget -qO- $URL | tar xJ -C sfdx --strip-components 1
 "./sfdx/install"
 export PATH=./sfdx/$(pwd):$PATH
@@ -70,7 +70,7 @@ if [ "$BRANCH" == "LEX" ]; then
   git diff --name-only master force-app/ |
   while read -r file; do
     # Copy the files from git diff into the deploy directory
-    sudo cp --parents "$file" $DEPLOYDIR
+    sudo cp --parents "$file" $DEPLOYDIR 2>/dev/null
     # For any changed class or trigger file, it's associated meta data file is copied to the deploy directory (and vice versa):
     if [[ $file == *.cls ]]; then
       find $classPath -samefile "$file-meta.xml" -exec sudo cp --parents -t $DEPLOYDIR {} + 2>/dev/null
