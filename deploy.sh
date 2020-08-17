cb576f6bd8c806e1ae0ca20aa99f173b5cc22c1a
Jack Barsotti <jackbarsotti1@gmail.com>
2020-08-17 12:36:20 -0500

    testing objectTranslation

--- deploy.sh
+++ deploy.sh
@@ -57,7 +57,7 @@ git config --global diff.renameLimit 9999999
 
 # Run a git diff for the incremental build depending on checked-out branch (if-statement per branch)
 # LEX branch:
-sudo rm -rf /Users/jackbarsotti/pgi_lex1/force-app/main/default/objectTranslations
+sudo rm -rf /Users/jackbarsotti/pgi_lex1/force-app/main/default/objectTranslations//Activity-bg/Activity-bg.objectTranslation-meta.xml
 sudo rm -rf /Users/jackbarsotti/pgi_lex1/force-app/main/default/objects
 if [ "$BRANCH" == "LEX" ]; then
   #create tracking branch
