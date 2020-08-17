8b439e9c9a721cf01d9bcb97e1c40f031097ce28
Jack Barsotti <jackbarsotti1@gmail.com>
2020-08-17 09:53:06 -0500

    testing objectTranslation

--- deploy.sh
+++ deploy.sh
@@ -88,7 +88,7 @@ if [ "$BRANCH" == "LEX" ]; then
       find $triggerPath -samefile "$parsedfile.trigger" -exec sudo cp --parents -t /Users/jackbarsotti/pgi_lex/force-app/main/default/diff {} + 2>/dev/null
     elif [[ $file == *.page ]]; then
       find force-app/main/default/pages -samefile "$file-meta.xml" -exec sudo cp --parents -t $DEPLOYDIR {} + 2>/dev/null
-    elif [[ $file == *Activity-bg.objectTranslation ]]; then
+    elif [[ $file == *.objectTranslation-meta.xml ]]; then
       find force-app/main/default/objectTranslations/Activity-bg -samefile "$file-meta.xml" -exec sudo cp --parents -t $DEPLOYDIR {}
     fi
   done
