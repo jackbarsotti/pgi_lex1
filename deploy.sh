70ed356db18d4f40e30b15997274ad689808fc22
Jack Barsotti <jackbarsotti1@gmail.com>
2020-08-17 07:55:54 -0500

    testing travis

--- deploy.sh
+++ deploy.sh
@@ -134,11 +134,11 @@ if [ "$BRANCH" == "LEX" ]; then
     export TESTLEVEL="RunSpecifiedTests -r $parsedList";
   fi;
 fi;
-id -un
+
 # Store our auth-url for our targetEnvironment alias for deployment
-sfdx force:auth:sfdxurl:store -f authtravisci.txt -a targetEnvironment
+sudo sfdx force:auth:sfdxurl:store -f authtravisci.txt -a targetEnvironment
 # Run apex tests and deploy apex classes/triggers
-sudo rm -rf /Users/jackbarsotti/pgi_lex1/force-app/main/default/diff/force-app/main/default/objectTranslations/Conference__c-en_US
+#sudo rm -rf /Users/jackbarsotti/pgi_lex1/force-app/main/default/diff/force-app/main/default/objectTranslations/Conference__c-en_US
 sudo rm -rf /Users/jackbarsotti/pgi_lex1/force-app/main/default/diff/force-app/main/default/objects
 sudo rm -rf /Users/jackbarsotti/pgi_lex1/force-app/main/default/diff/force-app/main/default/objectTranslations
 sudo rm -rf /Users/jackbarsotti/pgi_lex1/force-app/main/default/diff/force-app/main/default/pages/popUp.page
