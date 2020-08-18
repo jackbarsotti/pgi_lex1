f1f1f291cc40719d8ae9a7965a6febc3cd492ef9
Jack Barsotti <jackbarsotti1@gmail.com>
2020-08-18 13:02:16 -0500

    retrieve testing

--- retrieve.sh
+++ retrieve.sh
@@ -28,7 +28,7 @@ fi;
 # Set the target environment for force:source:retrieve command
 sfdx force:auth:sfdxurl:store -f authtravisci.txt -a targetEnvironment
 git remote update
-git fetch
+git pull
 git checkout --track origin/master
 git checkout LEX
 # Delete the contents of force-app folder before we paste source:retrieve contents into it
