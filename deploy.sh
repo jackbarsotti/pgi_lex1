556fec468a90789daad0a060559de9b3c2cfbf57
Jack Barsotti <jackbarsotti1@gmail.com>
2020-08-19 11:57:20 -0500

    test

--- deploy.sh
+++ deploy.sh
@@ -71,8 +71,8 @@ if [ "$BRANCH" == "LEX" ]; then
   git diff --diff-filter=UM --name-only master force-app/ |
   while read -r file; do
     # Copy the files from git diff into the deploy directory
-    #sudo cp --parents "$file" $DEPLOYDIR 2>/dev/null
-    #sudo cp -r --parents force-app/main/default/aura/ $DEPLOYDIR
+    sudo cp --parents "$file" $DEPLOYDIR
+
     # For any changed class or trigger file, it's associated meta data file is copied to the deploy directory (and vice versa):
     if [[ $file == *.cls ]]; then
       find $classPath -samefile "$file-meta.xml" -exec sudo cp --parents -t $DEPLOYDIR {} \;
