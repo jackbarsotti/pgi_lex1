178e211a1e14407a9202f5dab3fd8f5c58fb8fbb
Jack Barsotti <jackbarsotti1@gmail.com>
2020-08-19 08:27:22 -0500

    test

--- deploy.sh
+++ deploy.sh
@@ -91,6 +91,9 @@ if [ "$BRANCH" == "LEX" ]; then
       find $triggerPath -samefile "$parsedfile.trigger" -exec sudo cp --parents -t $DEPLOYDIR {} + 2>/dev/null
     elif [[ $file == *.page ]]; then
       find force-app/main/default/pages -samefile "$file-meta.xml" -exec sudo cp --parents -t $DEPLOYDIR {} + 2>/dev/null
+    elif [[ $file == *.page-meta.xml ]]; then
+      parsedfile=${file%.pages-meta.xml}
+      find force-app/main/default/pages -samefile "$parsedfile.pages" -exec sudo cp --parents -t $DEPLOYDIR {} +2>/dev/null
     fi
   done 
   echo 'Complete.'
