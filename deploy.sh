f7d970681aa4726e7ece1aac8fe386c2c47fd1b6
Jack Barsotti <jackbarsotti1@gmail.com>
2020-08-19 12:13:00 -0500

    test

--- deploy.sh
+++ deploy.sh
@@ -102,7 +102,7 @@ if [ "$BRANCH" == "LEX" ]; then
   ls $DEPLOYDIR/force-app/main/default
   echo
 fi;
-sudo cp force-app/main/default/diff/dlrs_ContactTrigger.trigger-meta.xml $DEPLOYDIR
+sudo cp force-app/main/default/triggers/dlrs_ContactTrigger.trigger-meta.xml $DEPLOYDIR
 ls /Users/jackbarsotti/pgi_lex1/force-app/main/default/diff/force-app/main/default/triggers
  
 # Make temporary folder for our <className>Test.cls files that will be parsed
