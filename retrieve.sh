58b0d9a72c243f5253ea588e6b7bb19e23f69c4a
Jack Barsotti <jackbarsotti1@gmail.com>
2020-08-19 15:46:52 -0500

    test

--- retrieve.sh
+++ retrieve.sh
@@ -31,8 +31,8 @@ sfdx force:auth:sfdxurl:store -f authtravisci.txt -a targetEnvironment
 git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
 git fetch -q
 #NEW
-git add .
-git commit -m "blah"
+##git add .
+##git commit -m "blah"
 git checkout master
 # Delete the contents of force-app folder before we paste source:retrieve contents into it
 echo
