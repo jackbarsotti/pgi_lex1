f46e723fed3926e0ba70d0a79faf3609c47ef6ea
Jack Barsotti <jackbarsotti1@gmail.com>
2020-08-19 12:08:52 -0500

    test

--- deploy.sh
+++ deploy.sh
@@ -67,12 +67,11 @@ if [ "$BRANCH" == "LEX" ]; then
   git checkout LEX
   echo
   echo 'Running a git diff, please wait...'
-  #this command is good, keeping us from deploying D files, making class dir smaller 
+
   git diff --diff-filter=UM --name-only master force-app/ |
   while read -r file; do
     # Copy the files from git diff into the deploy directory
     sudo cp --parents "$file" $DEPLOYDIR
-
     # For any changed class or trigger file, it's associated meta data file is copied to the deploy directory (and vice versa):
     if [[ $file == *.cls ]]; then
       find $classPath -samefile "$file-meta.xml" -exec sudo cp --parents -t $DEPLOYDIR {} \;
@@ -103,6 +102,7 @@ if [ "$BRANCH" == "LEX" ]; then
   ls $DEPLOYDIR/force-app/main/default
   echo
 fi;
+sudo cp force-app/main/default/diff/dlrs_ContactTrigger.trigger-meta.xml $DEPLOYDIR
 ls /Users/jackbarsotti/pgi_lex1/force-app/main/default/diff/force-app/main/default/triggers
  
 # Make temporary folder for our <className>Test.cls files that will be parsed
