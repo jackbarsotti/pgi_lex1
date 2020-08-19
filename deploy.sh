a36faf2ac52e31cd87502324277d352e9c678c26
Jack Barsotti <jackbarsotti1@gmail.com>
2020-08-19 14:36:23 -0500

    test M

--- deploy.sh
+++ deploy.sh
@@ -67,7 +67,7 @@ if [ "$BRANCH" == "LEX" ]; then
   git checkout LEX
   echo
   echo 'Running a git diff, please wait...'
-  #git diff --diff-filter=A --name-only master force-app/
+  git diff --diff-filter=M --name-only master force-app/
   git diff --diff-filter=UMA --name-only master force-app/ |
   while read -r file; do
     # Copy the files from git diff into the deploy directory
@@ -106,7 +106,7 @@ fi;
 #sudo rm -f /Users/jackbarsotti/pgi_lex1/force-app/main/default/diff/force-app/main/default/triggers/dlrs_ContactTrigger.trigger-meta.xml
 #sudo rm -f /Users/jackbarsotti/pgi_lex1/force-app/main/default/diff/force-app/main/default/triggers/dlrs_ContactTrigger.trigger
 #sudo cp --parents /Users/jackbarsotti/pgi_lex1/force-app/main/default/triggers/dlrs_ContactTrigger.trigger-meta.xml -t $DEPLOYDIR
-ls /Users/jackbarsotti/pgi_lex1/force-app/main/default/diff/force-app/main/default/triggers
+ls /Users/jackbarsotti/pgi_lex1/force-app/main/default/diff/force-app/main/default/classes
  
 # Make temporary folder for our <className>Test.cls files that will be parsed
 sudo mkdir -p /Users/jackbarsotti/pgi_lex1/force-app/main/default/unparsedTests
